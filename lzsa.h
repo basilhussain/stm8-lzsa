170246e8205d90af3d19d1a6ae9453fabd78f932
Basil Hussain <basilhussain@users.noreply.github.com>
2022-03-07 20:18:57 +0000

    Modify all library function declarations to specify old SDCC calling convention

--- lzsa.h
+++ lzsa.h
@@ -2,7 +2,7 @@
  *
  * lzsa.h - Header for LZSA decompression library
  *
- * Copyright (c) 2021 Basil Hussain
+ * Copyright (c) 2022 Basil Hussain
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -30,7 +30,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
-extern void * lzsa1_decompress_block(void *dst, const void *src);
-extern void * lzsa2_decompress_block(void *dst, const void *src);
+// Short-term fix to force usage of old ABI when compiled with SDCC v4.2.0
+// (or newer). New ABI passes simple arguments (e.g. one or two 8- or 16-bit
+// values) in A/X registers, versus previous where all args are on the stack.
+// Eventually, all assembly code should be changed to use new ABI. May actually
+// offer a performance increase, eliminating code that simply takes arg values
+// and loads them to A/X registers.
+#if defined(__SDCCCALL) && __SDCCCALL != 0
+#define __stack_args __sdcccall(0)
+#else
+#define __stack_args
+#endif
+
+extern void * lzsa1_decompress_block(void *dst, const void *src) __stack_args;
+extern void * lzsa2_decompress_block(void *dst, const void *src) __stack_args;
 
 #endif // LZSA_H_
